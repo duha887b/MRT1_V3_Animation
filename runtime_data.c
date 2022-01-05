cad937413e7ebf8bb4a98c556450e2ccbaa861c0
Dustin HAnusch <dustin.hanusch@mailbox.tu-dresden.de>
2022-01-05 16:35:22 +0100

    init runtime_data

---
+++ runtime_data.c
@@ -0,0 +1,9 @@
+//
+// Created by dustin on 05.01.22.
+//
+
+#include "runtime_data.h"
+
+void test(){
+    printf("hello data");
+}
---
+++ runtime_data.h
@@ -0,0 +1,13 @@
+//
+// Created by dustin on 05.01.22.
+//
+
+#ifndef MRT1_V3_ANIMATION_RUNTIME_DATA_H
+#define MRT1_V3_ANIMATION_RUNTIME_DATA_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void test();
+
+#endif //MRT1_V3_ANIMATION_RUNTIME_DATA_H
