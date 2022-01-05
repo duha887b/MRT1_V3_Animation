c7e1a2b888fafd5729d726fb9030d1b951f2c1b3
Dustin HAnusch <dustin.hanusch@mailbox.tu-dresden.de>
2022-01-05 18:56:24 +0100

    update runtime_data

--- runtime_data.c
+++ runtime_data.c
@@ -4,6 +4,146 @@
 
 #include "runtime_data.h"
 
-void test(){
-    printf("hello data");
+//initialisierung eines listelements
+void list_element_init(list_element *element) {
+    element->next = NULL;
+    element->payload = NULL;
+    element->typ = NULL;
 }
+//erstellt ein neues Listelement und initialisiert die daten
+list_element* list_element_new() {                              //erstellen eines neuen listelemts allokation des Speichers
+
+    list_element *new = (list_element *) malloc(sizeof(list_element));
+    list_element_init(new);
+    return (new);
+}
+//Initialisieren eines Listenkopfes
+void list_header_init(list_header *header) {
+    header->head = NULL;
+    header->tail = NULL;
+
+}
+
+// erstellt einen neuen Listenkopf (Liste)
+list_header *list_header_new() {    //erstellen eines neuen list headers
+    list_header *new = (list_header *) malloc(sizeof(list_header));
+    list_header_init(new);
+    return (new);
+}
+//löscht den inhalt eines listelemts und gibt optional den Speicher von payload frei
+void listelement_deleteMembers(list_element *element, _Bool freePayload) {
+
+    if(freePayload){
+        free(element->payload);
+    }
+    element->payload = NULL;
+    element->next = NULL;
+    element->typ = NULL;
+}
+
+//löscht das gesamte listelement und dessesn daten ( evt. deallokieren des Speichers)
+
+void listelement_delete(list_header *header, list_element *element, _Bool freePayload) {
+
+    list_element *tmp;
+    list_element *del;
+
+    tmp = header->head;
+
+    if (tmp == element) {            //delete head element
+
+        if(tmp->next == NULL){      // if head element is the last element
+            header->tail = NULL;
+            header->head = NULL;
+            goto next;
+        }
+
+        header->head = (list_element *) tmp->next;
+        goto next;
+    }
+
+    while (tmp->next != NULL) {
+        if (tmp->next == element) {
+
+            del = (list_element *) tmp->next;
+            tmp->next = del->next;
+
+            if (tmp->next == NULL) {
+                header->tail = tmp;
+            }
+
+            goto next;
+        }
+        tmp = tmp->next;
+    }
+
+    next:
+    listelement_deleteMembers(element,freePayload);         // clear or delete members of list_element
+    if(freePayload){
+        free(element);                                      //deallocate element himself
+        element = NULL;
+    }
+}
+
+// fügt das listelement einer Liste hinzu und dem listelement seine Daten
+void listelement_append(list_header *l, list_element *le, void *payload, typ *typ) {
+
+    // no elements
+    le->payload = payload;
+    le->next = NULL;
+    le->typ = typ;
+
+    if (l->head == NULL && l->tail == NULL){
+        l->head = le;
+        l->tail = le;
+        return;
+    }
+
+    // 1 element
+    if (l->head == l->tail) {
+        l->head->next = le;
+        l->tail = le;
+        return;
+    }
+
+    l->tail->next =  le;
+    l->tail = le;
+
+}
+//gesamter inhalt der Liste wird vom header gelöst (z.B zum neubefüllen der List)
+void list_clear(list_header *header) {
+    header->head = NULL;
+    header->tail = NULL;
+}
+
+//allokieren des speichers fürs array
+int* init_array(unsigned int row,unsigned int col){
+    int* array = (int *) malloc(row*col*sizeof (int ));
+    return array;
+}
+
+void del_array(int* array){
+    free(array);
+}
+
+//einfügen von daten an bestimmten punkt (0,0 erste position) //TODO non functional
+void insert_data_array(int* array,int* dimY,int dimX,unsigned int row,unsigned int col,int payload){
+    *(array + row* (dimX) + col) = payload;
+}
+//TODO non functional
+int* get_array_data(int* array,int* dimY,int dimX,unsigned int row,unsigned int col){
+    return (int *) (array + row * (dimX) + col);
+}
+/*
+int main(){
+    int *arr = init_array(2,2);
+    insert_data_array(arr,0,0,23);
+    insert_data_array(arr,0,1,7);
+    insert_data_array(arr,1,0,13);
+    insert_data_array(arr,1,1,9);
+
+    printf("\n%d", *(get_array_data(arr,0,0)));
+
+    return 0;
+}
+*/
--- runtime_data.h
+++ runtime_data.h
@@ -8,6 +8,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void test();
+
+// List element typen für auswertung des Pointers
+/*
+ * 0 X --> Spalten (int)
+ * 1 Y --> Zeilen (int)
+ * 2 AN_COUNTER --> derzeitiger Animationsschritt (int)
+ * 3 AN_MAX --> maximale Anzahl an Animationsschritten (int)
+ * 4 DELAY --> Pause zwischen Animationsschritten in ms (long)
+ * 5 ARRAY --> Liste die das Array des Animationspuffers beinhaltet (list)
+ * 6 VOID --> Zusatzinformationen (void)
+ */
+typedef enum {
+    X,Y,AN_COUNTER,AN_MAX,DELAY,ARRAY,VOID
+}typ;
+
+//Inhalt eines List elements
+typedef struct list_el_t{
+    struct list_el_t *next;
+    void *payload;
+    typ *typ;
+}list_element;
+
+//struktur List header
+typedef struct list_t{
+    list_element *head;
+    list_element *tail;
+}list_header;
+
+//Schnittstelle
+
+//erstellen einer Liste mit den Elemnten (X,Y,AN_COUNTER,AN_MAX,DELAY,ARRAY)
+list_header* new_specific_list();
+
+// anderes Element des typs void der Liste hinzufügen
+//void add_listElement(list_element* le);
+
+
+
+//getter setter X-Achse
+int get_X(list_header* list);
+void set_X(list_header* list, int* x);
+
+//getter setter Y-Achse
+int get_Y(list_header* list);
+void set_Y(list_header* list, int* y);
+
+//getter setter Animationszähler
+int get_animation_counter(list_header* list);
+void set_animation_counter(list_header* list, int* counter);
+
+//getter setter maximale Animationsschritte
+int get_animation_maxC(list_header* list);
+void set_animation_maxC(list_header* list, int* maxC);
+
+//getter setter Pause zwischen Animationsschritten
+long get_delay(list_header* list);
+void set_delay(list_header* list, long* delay);
+
+//definieren des Animationspuffers in der Liste der Daten mit informationen der Liste
+void def_animation_array(list_header * list); //TODO 3.2
+void replace_element_array();
+void delete_array();
 
 #endif //MRT1_V3_ANIMATION_RUNTIME_DATA_H
