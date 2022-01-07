4c8727dad293e6be4643b4f04a2586f28d015f4e
Dustin HAnusch <dustin.hanusch@mailbox.tu-dresden.de>
2022-01-07 20:01:35 +0100

    update config

--- config.c
+++ config.c
@@ -7,11 +7,52 @@
 
 // für fopen, getline, fscanf, fclose:
 #include <stdio.h>
-
 // für strncmp und memset:
 #include <string.h>
-
 // für malloc, calloc und atoi:
 #include <stdlib.h>
-	 
 // Config und Dateiarbeit Funktionen
+#define _GNU_SOURCE
+
+void read_settings(list_header *list,char* path){
+    FILE *file;
+    file = fopen(path,"r");
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+
+
+    // Fehlererkennung
+    unsigned long length = strlen(path);
+    const char *last_four = &path[length-4];           //Bestimmung letzte 4 Zeichen des Dateinamens
+
+
+    if(strcmp(last_four,".txt") != 0){
+        fprintf(stderr,"\nfile is no .txt\n");
+        return;
+    }
+
+    perror("config");
+
+    if(file == NULL){
+        fprintf( stderr, "\nfile (%s) doesn't exist\n",path);
+        return;
+    }
+
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+        fscanf();
+
+
+    }
+
+
+
+
+
+
+}
--- config.h
+++ config.h
@@ -3,4 +3,9 @@
 
 // Config Schnittstelle
 
+#include "runtime_data.h"
+
+// einlesen der settings.txt in runtime_data
+void read_settings(list_header *list,char* path);
+
 #endif //CONFIG_HH
