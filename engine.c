43c283bd6728286fa19989bb1edb297a3bccbb7e
Dustin HAnusch <dustin.hanusch@mailbox.tu-dresden.de>
2022-01-08 16:17:00 +0100

    engine.c (prototype done)

--- engine.c
+++ engine.c
@@ -8,33 +8,103 @@
 
 //TODO get pixel of matrix --> add around pixels --> check
 
+
+
 void cal_nextAnimaionStep(list_header* list){
 
+    int *current_array = get_array(list);       //tn
+    int *next_array = new_array(list);          //tn + 1
 
-   int *current_array = get_array(list);       //tn
-   int *next_array = new_array(list);          //tn + 1
+    int cols = get_X(list) + 2;                 //dimensionen des arrays
+    int rows = get_Y(list) + 2;
 
+    int* counter = get_animation_counter(list); //pointer des Animationscounters holen
 
-   int cols = get_X(list) + 2;                 //dimensionen des arrays
-   int rows = get_Y(list) + 2;
+/*
+   // Temporärer code
+   for (int i = 0; i < rows * cols; i++)
+        current_array[i] = 0;
 
-   printf("X:%d,Y:%d", get_X(list), get_Y(list));
-
-
-
-
-
-    //if randpixel
-
-    //else
-
-
-
+   //current_array[m*cols+n]
+    current_array[1*cols+0] = 1;
+    current_array[1*cols+1] = 1;
+    current_array[2*cols+0] = 1;
+    current_array[2*cols+1] = 1;
 
 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            printf("%d ", current_array[i * cols + j]);
+        printf("\n");
+    }
+    */
 
 
-   // set_array(list,next_array);
 
+   for(int r = 0;r<rows;r++){
+       for(int c = 0; c<cols ;c++){
+           //r,c iter über alle Elemente der matrix r,c
+            //temporärer zähler der belegten Nachbarn
+
+           //iterieren über die nachbarn des pixels m,n
+           int tmp_count = 0;
+           for(int m = r-1; m<=r+1 ;m++ ){
+               for(int n = c-1; n<=c+1 ;n++){
+
+                   if((n==c && m==r)){
+                       continue;
+                   }else if(n<0 || m<0){
+                       continue;
+                   }else if(n>cols || m>rows){
+                       continue;
+                   }
+
+                   tmp_count = tmp_count + (current_array[m*cols+n]);
+               }
+           }
+
+
+           // set new pixel in next_array
+
+           if(current_array[r*cols+c] == 0 && tmp_count == 3){
+               next_array[r*cols+c] = 1;
+               continue;
+           }else if(current_array[r*cols+c] == 1 && (tmp_count == 3||tmp_count == 2)) {
+               next_array[r * cols + c] = 1;
+               continue;
+           }else if(current_array[r*cols+c] == 1 && tmp_count < 2) {
+               next_array[r * cols + c] = 0;
+               continue;
+           }else if(current_array[r*cols+c] == 1 && tmp_count > 3) {
+               next_array[r * cols + c] = 0;
+               continue;
+           }
+
+       }
+   }
+/*
+   printf("_____________________\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            printf("%d ", next_array[i * cols + j]);
+        printf("\n");
+    }
+*/
+    *counter = *counter+1;
+    current_array = next_array;
+/*
+    printf("_____________________\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            printf("%d ", current_array[i * cols + j]);
+        printf("\n");
+    }
+        printf("counter:%d",*counter);
+        */
 }
+
+
+
 
